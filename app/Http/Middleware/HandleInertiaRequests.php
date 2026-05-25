9b9e27d7c02c1679c679fd3254df9d05d618703a
Benjamin Shaw <benshawrob@gmail.com>
2026-05-25 22:42:13 +0200

    feat: share unread_messages_count as global Inertia prop

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -42,6 +42,7 @@ class HandleInertiaRequests extends Middleware
                 'user' => $request->user(),
             ],
             'unread_notifications_count' => $request->user()?->unreadNotifications()->count() ?? 0,
+            'unread_messages_count' => $request->user()?->unreadMessagesCount() ?? 0,
             'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
         ];
     }
