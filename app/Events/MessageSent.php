c81b8195328b7623158a516fe6bebb38724ca0df
Benjamin Shaw <benshawrob@gmail.com>
2026-05-25 22:33:44 +0200

    feat: add MessageSent and UserTyping broadcast events

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageSent implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(
+        public Message $message,
+        public int $recipientId,
+    ) {
+        $message->load('sender');
+    }
+
+    public function broadcastOn(): array
+    {
+        return [new PrivateChannel('user.'.$this->recipientId)];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'MessageSent';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'message' => [
+                'id' => $this->message->id,
+                'conversation_id' => $this->message->conversation_id,
+                'body' => $this->message->body,
+                'sender_id' => $this->message->sender_id,
+                'sender' => [
+                    'id' => $this->message->sender->id,
+                    'name' => $this->message->sender->name,
+                    'username' => $this->message->sender->username,
+                    'avatar_url' => $this->message->sender->avatar_url,
+                ],
+                'created_at' => $this->message->created_at->diffForHumans(),
+                'is_mine' => false,
+            ],
+        ];
+    }
+}
---
+++ app/Events/UserTyping.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class UserTyping implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(
+        public int $conversationId,
+        public int $senderId,
+        public int $recipientId,
+    ) {}
+
+    public function broadcastOn(): array
+    {
+        return [new PrivateChannel('user.'.$this->recipientId)];
+    }
+
+    public function broadcastAs(): string
+    {
+        return 'UserTyping';
+    }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'conversation_id' => $this->conversationId,
+            'sender_id' => $this->senderId,
+        ];
+    }
+}
